cd01dbb29d723834a894120adccac8d8a995d8fa
Pratyaksh0610 <pratyaksh0610@gmail.com>
2025-11-21 09:56:27 +0530

    assertion-nonNull operators

---
+++ typeScript/assertion-optional/assertion.ts
@@ -0,0 +1,16 @@
+// type Props = {
+//     name: string;
+//     class: string;
+//     age?: number;
+// }
+
+const Student : any = {
+    name : "Alice",
+    class: "10th Grade"
+}
+console.log(Student.name)
+console.log(Student.age);
+
+if(Student.age > 18){
+    console.log("Adult");
+}
